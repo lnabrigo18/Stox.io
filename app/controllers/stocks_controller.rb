d6e69a196569d814b00dba9a6a50218d440e6d91
lnabrigo18 <lnabrigo@gmail.com>
2023-06-22 13:35:24 +0800

    update api url

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -19,10 +19,10 @@ class StocksController < ApplicationController
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex_client[:publish_access_key],
       secret_token: Rails.application.credentials.iex_client[:secret_access_key],
-      endpoint: 'https://sandbox.iexapis.com/v1'
+      endpoint: 'https://cloud.iexapis.com/v1'
     )
     symbols = client.ref_data_symbols
-    nas_symbols = symbols.select { |symbol| symbol.exchange == 'NAS' }
+    nas_symbols = symbols.select { |symbol| symbol.exchange == 'XNYS' }
     @selected_symbols = (0...nas_symbols.length).select { |x| x % 30 == 30 - 1 }.map { |y| nas_symbols[y] }
     @selected_symbols.each do |stock|
       Stock.find_or_create_by('ticker' => stock.symbol, 'company' => client.company(stock.symbol).company_name, 'change_percent' => client.quote(stock.symbol).change_percent_s, 'price' => client.price(stock.symbol))
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,9 +4,11 @@ class Stock < ApplicationRecord
   validates :change_percent, presence: true
   validates :price, presence: true
   def self.new_lookup(ticker_symbol)
-    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:publish_access_key],
-                                  secret_token: Rails.application.credentials.iex_client[:secret_access_key],
-                                  endpoint: 'https://sandbox.iexapis.com/v1')
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex_client[:publish_access_key],
+      secret_token: Rails.application.credentials.iex_client[:secret_access_key],
+      endpoint: 'https://cloud.iexapis.com/v1'
+    )
     new(ticker: ticker_symbol, company: client.company(ticker_symbol).company_name, change_percent: client.quote(ticker_symbol).change_percent_s, price: client.price(ticker_symbol))
   end
 end
